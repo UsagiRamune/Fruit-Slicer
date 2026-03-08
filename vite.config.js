3f2bd3ad561b651c070c03278aed7dfa9d98bfb1
K1zumi <ggotliliana@gmail.com>
2026-03-09 01:22:47 +0700

    Fix Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        versionA: './versionA.html',
-        versionB: './versionB.html'
-      }
-    }
   }
 })
