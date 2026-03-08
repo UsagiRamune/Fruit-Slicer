6736e60c86de3d7ebc32fff1937b5083fcd834b2
K1zumi <ggotliliana@gmail.com>
2026-03-09 04:40:58 +0700

    Hotfix#2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: './',
   build: {
-    outDir: 'dist',
-  }
-})
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),      // หน้าเกมหลัก
+        admin: resolve(__dirname, 'admin.html'),    // หน้า Dashboard ที่มึงต้องการ
+      },
+    },
+  },
+});
